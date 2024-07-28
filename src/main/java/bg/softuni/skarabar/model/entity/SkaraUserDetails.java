423eeaf5935eaa086fffea6099582fbcf2a0aaaf
996r <deadant2001@gmail.com>
2024-07-28 10:27:41 +0300

    1.Added SkaraUserDetails which extends User/Spring Security/

---
+++ src/main/java/bg/softuni/skarabar/model/entity/SkaraUserDetails.java
@@ -0,0 +1,35 @@
+package bg.softuni.skarabar.model.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class SkaraUserDetails extends User {
+
+    private final String firstName;
+    private final String lastName;
+
+
+    public SkaraUserDetails
+            (String username,
+             String password,
+             Collection<? extends GrantedAuthority> authorities, String fistName, String lastName) {
+        super(username,
+                password, authorities);
+        this.firstName = fistName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName(){
+        return firstName+lastName.toString();
+    }
+}
